597e98f438b3c57bcbef21be215764f2a70a9899
Jose Esteban Calle <jcallec7@hotmail.com>
2022-04-24 17:37:44 -0500

    Primer commit

---
+++ useCouter.js
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+
+
+export const useCouter = ( initialState = 10 ) => {
+    
+    const [counter, setCounter] = useState(initialState); // 10
+
+    const reset = () => {
+        setCounter( initialState );
+    }
+
+    const increment = (factor = 1) => {
+        setCounter( counter + factor );
+    }
+
+    const decrement = (factor = 1) => {
+        setCounter( counter - factor );
+    }
+
+    return {
+        counter,
+        increment,
+        decrement,
+        reset
+    };
+
+}
